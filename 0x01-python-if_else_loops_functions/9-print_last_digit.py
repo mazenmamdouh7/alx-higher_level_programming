623ca1cc3cb8c70568e21a76101b76b5b695543a
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2023-10-31 16:50:49 -0700

    Python

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     last_digit = abs(number) % 10
-    print_last_digit = __import__('9-print_last_digit').print_last_digit
+    print('{:02d}'.format(last_digit))
     return last_digit
