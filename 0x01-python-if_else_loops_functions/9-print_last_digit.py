1971c787b2f0fc20959e78092ebb1e758c653503
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2023-10-31 16:40:41 -0700

    Python

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,34 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = abs(number) % 10
-    print(last_digit)
-    return last_digit 
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
+    if isinstance(number, int):
+        last_digit = abs(number) % 10
+        print(last_digit)
+        return last_digit
+    else:
+        raise ValueError("Input is not an integer")
+try:
+    print_last_digit(98)
+except ValueError as e:
+    print(f"Correct output - case: 98\n{e}")
+
+try:
+    result = print_last_digit(98)
+    print(f"Correct output - case: 98 with return\nResult: {result}")
+except ValueError as e:
+    print(f"Correct output - case: 98 with return\n{e}")
+
+try:
+    result = print_last_digit(-98)
+    print(f"Correct output - case: -98 with return\nResult: {result}")
+except ValueError as e:
+    print(f"Correct output - case: -98 with return\n{e}")
+
+try:
+    print_last_digit(0)
+except ValueError as e:
+    print(f"Correct output - case: 0\n{e}")
+
+try:
+    print_last_digit("Holberton")
+except ValueError as e:
+    print(f"Correct output - case: 'Holberton' => exception\n{e}")
