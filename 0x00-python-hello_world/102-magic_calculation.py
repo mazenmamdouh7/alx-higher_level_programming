affc6e2421afd802de3b15c009418f5c3d865e87
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2023-10-31 07:38:52 -0700

    advanced

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,6 +1,6 @@
 def magic_calculation(a, b):
-    value = 98
-    value += a
-    value = value ** b
-    value += b
-    return value
+    result = 98
+    result += a
+    result = result ** b
+    result += b
+    return result
