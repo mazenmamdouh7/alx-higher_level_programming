4348d4fcb6e4a9db5be4540c09fbc1b7163826b2
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2023-10-31 16:27:11 -0700

    Python

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    last_digit = abs(number) % 10  # Get the absolute value of the number and find the remainder when divided by 10 (to get the last digit)
-    print(last_digit)  # Print the last digit
-    return last_digit  # Return the last digit
+print_last_digit = __import__('9-print_last_digit').print_last_digit
 
-# Example usage:
 print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
