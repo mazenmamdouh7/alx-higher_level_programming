f2380f04391a19820279840430c04bebb041b581
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2023-10-31 17:03:58 -0700

    Python

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+from decimal import Decimal
+
 def pow(a, b):
+    a = Decimal(str(a))
+    b = Decimal(str(b))
     if b == 0:
         return 1
     if b < 0:
-        a = 1 / a
+        a = Decimal(1) / a
         b = -b
-    result = 1
+    result = Decimal(1)
     while b > 0:
         if b % 2 == 1:
             result *= a
         a *= a
         b //= 2
-    return round(result, 2)
+    return result
