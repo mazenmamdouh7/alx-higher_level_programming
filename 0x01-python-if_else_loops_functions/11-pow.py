1d86e6a5c51bc282b12c48ce3453da144ccd8d66
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2023-10-31 17:02:35 -0700

    Python

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result = 1
+    if b == 0:
+        return 1
     if b < 0:
         a = 1 / a
         b = -b
-    for _ in range(b):
-        result *= a
-    return result
+    result = 1
+    while b > 0:
+        if b % 2 == 1:
+            result *= a
+        a *= a
+        b //= 2
+    return round(result, 2)
