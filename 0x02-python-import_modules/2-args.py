203837044dd7ec22f070574dda796dba707dbaa0
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2023-11-02 15:17:10 -0700

    Python

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+counter = len(sys.argv)
+if  counter == 0:
+    print("0  .")
+
+elif counter == 1:
+    print("1 argument: ")
+
+else:
+    print("{} arguments: ".format(counter))
+
+for num in range(counter):
+    print("{}: {}".format(num+1,sys.argv[num+1]))
