bfa14520968da6c3930173a1dfdddc25404c3ebc
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2023-11-02 15:20:58 -0700

    Python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,12 +6,9 @@ if __name__ == "__main__":
     counter = len(sys.argv)
     if  counter == 0:
         print("0  .")
-
     elif counter == 1:
         print("1 argument: ")
-
     else:
         print("{} arguments: ".format(counter))
-
     for num in range(counter):
         print("{}: {}".format(num+1,sys.argv[num+1]))
