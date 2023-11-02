60cc92fb72b6257012c7f9bc35ce8a47b58b2494
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2023-11-02 15:23:09 -0700

    Python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     
     import sys
     
-    counter = len(sys.argv)
+    counter = len(sys.argv)-1
     if  counter == 0:
-        print("0  .")
+        print("0 arguments.")
     elif counter == 1:
         print("1 argument: ")
     else:
