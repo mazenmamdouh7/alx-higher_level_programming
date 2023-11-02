22566644beb01a982a471fcc4521a8a2cabb5b17
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2023-11-02 15:19:12 -0700

    Python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
-counter = len(sys.argv)
-if  counter == 0:
-    print("0  .")
+if __name__ == "__main__":
+    import sys
+    counter = len(sys.argv)
+    if  counter == 0:
+        print("0  .")
 
-elif counter == 1:
-    print("1 argument: ")
+    elif counter == 1:
+        print("1 argument: ")
 
-else:
-    print("{} arguments: ".format(counter))
+    else:
+        print("{} arguments: ".format(counter))
 
-for num in range(counter):
-    print("{}: {}".format(num+1,sys.argv[num+1]))
+    for num in range(counter):
+        print("{}: {}".format(num+1,sys.argv[num+1]))
