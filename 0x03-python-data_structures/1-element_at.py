a88538ca33c6abde12291264134263b532578681
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2023-11-04 08:19:00 -0700

    Python

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    long_of_list = len(my_list) - 1
-    if (idx < 0 or idx > long_of_list):
-        return(None)
+    listlength = len(my_list) - 1
+    if (idx < 0 or idx > listlength):
+        return (None)
     else:
-        return(my_list[idx])
+        return (my_list[idx])
