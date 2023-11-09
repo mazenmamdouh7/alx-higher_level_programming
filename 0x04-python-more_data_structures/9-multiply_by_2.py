50bf43045c9ce5355eee31e05f05fae946610432
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2023-11-08 17:45:23 -0800

    Python

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dirct = {}
+    new_di = {}
     for num in a_dictionary:
         new_d[num] = a_dictionary[num] * 2
-    return new_dirct
+    return new_d
