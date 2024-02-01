ce9996f2331ea3fad03053675bf72e1c48cec763
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2024-01-31 18:25:00 -0800

    Python

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 x = 0
 for char in range(ord("z"), ord("a")-1, -1):
-    if char == ord("z"):
+    if char % 2 == 0:
         print("{}".format(chr(char)), end="")
     else:
         print("{}".format(chr(char - x)), end="")
