e9900a69fb7b846e63af4040d0230c509409da59
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2024-02-02 14:24:59 -0800

    Python

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,6 @@
 if __name__ == "__main__":
     import sys
     import hidden_4
-    char = 0
-    while char <= dir(hidden_4):
+    for char in dir(hidden_4):
         if char[:2] != "__":
             print(char)
-            char += 1
