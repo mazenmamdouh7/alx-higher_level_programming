ad6ab2c2537eb327f81f6849d12c8d078864dd14
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2024-02-03 20:14:36 -0800

    Python - Data Structures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,8 @@
-#!/usr/bin/python4
-def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 1 and len(matrix[0]) == 0:
-        print("")
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        print("", end="")
     else:
-        for arr in matrix:
-            for m in arr:
-                if m != arr[-1]:
-                    print("{:d}".format(m), end=" ")
-                else:
-                    print("{:d}".format(m))
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
