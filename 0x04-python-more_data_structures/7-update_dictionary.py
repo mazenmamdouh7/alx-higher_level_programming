c050da9c8286e93bef1f3d26424b97973433f566
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2024-02-08 13:57:25 -0800

    Python - More Data Structures

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     a_dictionary.update({key: value})
-    for item in a_dictionary:
-        print("{}: {}".format(item, a_dictionary[item]))
-    exit(0)
+    return (a_dictionary)
