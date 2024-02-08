e925e73c17a39b5454e39d6b61b200b82d3b85ba
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2024-02-08 14:21:52 -0800

    Python - More Data Structures

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-    return (a_dictionary)
+    if key not in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        for member in a_dictionary:
+            if member == key:
+                a_dictionary[member] = value
+    return a_dictionary
