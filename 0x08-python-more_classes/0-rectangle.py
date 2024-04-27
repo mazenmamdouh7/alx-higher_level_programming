a8ea93a2532d704442480ad79ee5f9c8e324b13b
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2024-04-27 00:49:02 -0700

    Python

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Define an empty class Rectangle"""
+"""Define an empty class Rectangle."""
+
 
 class Rectangle:
-"""Class that define a properties of rectangle"""
+    """Class that define a properties of rectangle."""
     pass
