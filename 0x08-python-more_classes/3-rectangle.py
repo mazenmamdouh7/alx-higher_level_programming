454982ba7a85ed633d8ceacd16c114dc1881170e
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2024-04-28 05:07:17 -0700

    Python

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -79,8 +79,8 @@ class Rectangle:
         """
         if (self.__width == 0) or (self.__height == 0):
             return ""
-        
+
         for i in range(self.__height):
-            print("#"* (self.__width))
+            print("#" * (self.__width))
             if i != self.__height - 1:
                 rect.append("\n")
