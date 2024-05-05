84a8daad495c3aeb3b3d8fbc7ee66b7098ffb996
root <mazenmamdouh7>
2024-05-05 13:30:57 -0700

    Input/Output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """This module defines a text file-writing function."""
 
+
 def write_file(filename="", text=""):
-    """writes a string to a text file (UTF8) 
+    """writes a string to a text file(UTF8)
     and returns the number of characters written.
     """
     with open(filename, "w", encoding="utf-8") as fw:
         return fw.write(text)
-
