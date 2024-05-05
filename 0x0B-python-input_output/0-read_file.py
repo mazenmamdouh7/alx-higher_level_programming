87ed3a7b4e3d8567bbe36d837a8ecdfd0ae27d4d
root <mazenmamdouh7>
2024-05-05 13:16:36 -0700

    Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
+"""This module defines a text file-reading function"""
+
 
 def read_file(filename=""):
-    """
-    Function that reads a text file (UTF8) and prints it to stdout.
-
-    Args:
-        filename: the Name of the file.
-    Returns:
-        The input of the file.
-    """
-    with open(filename, "r", encoding="utf-8") as file:
-        print(file.read(), end="")
+    """Prints the contents of a UTF8 text file"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
