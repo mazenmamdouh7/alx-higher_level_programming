c5fd02bd2f9befbfe69b56affc97233cb7772a2f
root <mazenmamdouh7>
2024-05-05 12:06:38 -0700

    Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    try:
-        with open(filename, 'r', encoding='UTF8') as file:
-            data = file.read()
-            print(data)
-    except FileNotFoundError:
-        print("file doesn't exist")
+    with open(filename, 'r', encoding='UTF8') as f:
+        for line in f:
+            print(line, end="")
