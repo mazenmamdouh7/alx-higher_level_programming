650dd97a7ccd3ae338970d9db868ef4405ec3006
root <mazenmamdouh7>
2024-05-06 18:05:32 -0700

    Input/Output

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This module defines a class Student"""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Gets a dictionary representation of the Student"""
+        return self.__dict__
+
+    obj_dict = {}
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student
+        If attrs is a list of strings,
+        represents only those attributes
+        included in the list
+        """
+        if type(attrs) == list and
+        all(isinstance(attr, str) for attr in attrs):
+            for key in attrs:
+                if hasattr(self, key):
+                    obj_dict[key] = getattr(self, key)
+        else:
+            obj_dict = self.__dict__.copy()
+        return obj_dict
