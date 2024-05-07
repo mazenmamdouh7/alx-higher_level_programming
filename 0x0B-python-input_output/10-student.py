9e003eb697fec291c883f5d0cc5736a18a447a4b
root <mazenmamdouh7>
2024-05-06 18:10:28 -0700

    Input/Output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -6,24 +6,18 @@ class Student:
     """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student"""
+        """Initializes a new Student
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    obj_dict = {}
-
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student
-        If attrs is a list of strings,
-        represents only those attributes
+        If attrs is a list of strings, represents only those attributes
         included in the list
         """
-        if type(attrs) == list and
-        all(isinstance(attr, str) for attr in attrs):
-            for key in attrs:
-                if hasattr(self, key):
-                    obj_dict[key] = getattr(self, key)
-        else:
-            obj_dict = self.__dict__.copy()
-        return obj_dict
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
