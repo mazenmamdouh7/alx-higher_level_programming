1b499e61ddfe3478b362f9376e12583a3db85057
root <mazenmamdouh7>
2024-07-02 22:42:19 -0700

    Almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,7 +77,8 @@ class Rectangle(Base):
             print("#" * self.width)
 
     def __str__(self):
-        """method so that it returns [Rectangle] 
+        """method so that it returns [Rectangle]
         (<id>) <x>/<y> - <width>/<height>
         """
-        return f"({self.id}) {self.x}/{self.y}-{self.width}/{self.height}"
+        return f"([Rectangle] {self.id})
+        {self.x}/{self.y}-{self.width}/{self.height}"
