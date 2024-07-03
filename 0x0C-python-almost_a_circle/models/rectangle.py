61a4140a5c1e649f483d47e6344ea2078470c6fe
root <mazenmamdouh7>
2024-07-02 22:45:40 -0700

    Almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -80,4 +80,4 @@ class Rectangle(Base):
         """method so that it returns [Rectangle]
         (<id>) <x>/<y> - <width>/<height>
         """
-        return f"([Rectangle]({self.id}){self.x}/{self.y}-{self.width}/{self.height}"
+        return f"([Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
