19e28b4741fb6f1ffbd7ce2ba2352e8be88012a4
root <mazenmamdouh7>
2024-07-04 01:45:57 -0700

    Almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,7 +15,8 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
+    
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None or not list_dictionaries:
