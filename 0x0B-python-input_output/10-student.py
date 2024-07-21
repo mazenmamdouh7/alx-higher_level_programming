420353ed120d98627ba409bd59ef359ca56f9180
root <mazenmamdouh7>
2024-07-20 18:06:56 -0700

    Python

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -4,8 +4,10 @@
 
 class Student:
     """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
+
         Args:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
@@ -17,20 +19,22 @@ class Student:
 
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
+
         If attrs is a list of strings, represents only those attributes
         included in the list.
+
         Args:
             attrs (list): (Optional) The attributes to represent.
         """
-        if (type(attrs) == list and
-                all(isinstance(ele, str) for ele in attrs)):
+        if isinstance(attrs, list) and all(isinstance(ele, str) for ele in attrs):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
 
     def reload_from_json(self, json):
-        """Replace all attributes of the Student.
+        """Replace all attributes of the Student instance.
+
         Args:
             json (dict): The key/value pairs to replace attributes with.
         """
-        for k, v in json.items():
-            setattr(self, k, v)
+        for key, value in json.items():
+            setattr(self, key, value)
